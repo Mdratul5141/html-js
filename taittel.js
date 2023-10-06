499456bd7611589572e266aedf1f73ae5c5dd928
mafuzur rahamen <mdratulhasun9084@gamil.com>
2023-10-06 14:55:22 +0600

    mafuz js

---
+++ taittel.js
@@ -0,0 +1,28 @@
+document.getElementById("tohin").innerText="how ar you";
+var y,x,z;
+y=20;
+x=30;
+z=y+x;
+document.write(z)
+//variable
+let rakib="how ar you"
+document.write(rakib)
+
+
+let answer1 ="how ar you";
+let answer2= "my name is a mafug"; 
+let answer3 ="i am class 20";
+document.getElementById("rana").innerHTML = answer1+ "<br>" + answer2 + "<br>" + answer3;
+
+
+
+<button onclick="displayDate()">The time is not?</button>
+function displayDate() {
+    document.getElementById("ttt").innerHTML = Date();
+  }
+
+
+
+
+
+
